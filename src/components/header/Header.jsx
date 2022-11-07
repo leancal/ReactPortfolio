2f0bb6ed0c737aff20f5934367b0be1ee7921f73
Leandro Calvet <leandrocalvet14@gmail.com>
2022-11-07 18:21:45 -0300

    design

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./header.css";
 import CTA from "./CTA";
 import ME from "../../assets/img/me.png";
+import HeaderSocial from "./HeaderSocials";
 
 const Header = () => {
   return (
@@ -11,6 +12,7 @@ const Header = () => {
         <h1>Leandro Calvet</h1>
         <h5 className="text-light">FullStack Developer</h5>
         <CTA />
+        <HeaderSocial />
 
         <div className="me">
           <img src={ME} alt="me" />
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import{BsLinkedin} from 'react-icons/bs'
+import{GoMarkGithub} from 'react-icons/go'
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://www.linkedin.com/in/leandrocalvet/" target="_blank"></a>
-        <a href="https://github.com/leancal" target="_blank"></a>
+        <a href="https://www.linkedin.com/in/leandrocalvet/" target="_blank"><BsLinkedin/></a>
+        <a href="https://github.com/leancal" target="_blank"><GoMarkGithub/></a>
     </div>
   )
 }
