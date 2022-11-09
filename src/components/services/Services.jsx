94b9d2d1fc84ef1628cfcf0efea2f5f8b7e42875
Leandro Calvet <leandrocalvet14@gmail.com>
2022-11-09 18:45:22 -0300

    SERVICES

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -46,6 +46,86 @@ function Services() {
           </ul>
         </article>
         {/* END OF UX/UI */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing eli</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing eli</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing eli</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing eli</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing eli</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing eli</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing eli</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing eli</p>
+            </li>
+            
+          </ul>
+        </article>
+        {/* END WEB DEVELOPER */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Content Creation</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing eli</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing eli</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing eli</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing eli</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing eli</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing eli</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing eli</p>
+            </li>
+            
+          </ul>
+        </article>
+        {/* END OF CONTENT CREATION */}
       </div>
     </section>
   );
