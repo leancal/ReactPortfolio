6d2cf641b9621b45f133ba6d01ea14a758bfa285
Leandro Calvet <leandrocalvet14@gmail.com>
2022-11-09 13:18:28 -0300

    SERVICES

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
-import './services.css'
-
+import React from "react";
+import "./services.css";
+import {BiCheck} from "react-icons/bi";
 
 function Services() {
   return (
-    <section id='services'>Services</section>
-  )
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>UX/UI Design</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing eli</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing eli</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing eli</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing eli</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing eli</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing eli</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing eli</p>
+            </li>
+            
+          </ul>
+        </article>
+        {/* END OF UX/UI */}
+      </div>
+    </section>
+  );
 }
 
-export default Services
+export default Services;
